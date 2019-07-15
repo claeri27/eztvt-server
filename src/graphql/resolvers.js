ab44425b853f93302d6cb3547178ca1ad4aa908b
Christopher Laeri <claeri27@gmail.com>
2019-07-15 15:03:04 -0400

    Added Show and Shows query

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -7,7 +7,71 @@ module.exports = {
       return db.User.findByPk(args.id)
     },
     Show: async (parents, args, context, info) => {
-      return db.Show.findByPk(args.id)
+      const key = Object.keys(args)[0]
+      if (key === 'name') {
+        return db.Show.findOne({ where: { name: args.name } })
+      } else {
+        return db.Show.findByPk(args.id)
+      }
+    },
+    Shows: async (parents, args, context, info) => {
+      const keys = Object.keys(args)
+      if (keys.length === 1) {
+        switch (keys[0]) {
+          case 'network':
+            return db.Show.findAll({
+              where: { network: args.network },
+            })
+          case 'status':
+            return db.Show.findAll({
+              where: { status: args.status },
+            })
+          case 'country':
+            return db.Show.findAll({
+              where: { country: args.country },
+            })
+        }
+      } else if (keys.length === 2) {
+        console.log('KEY0: ', keys[0], 'KEY1: ', keys[1])
+        if (keys[0] === 'network') {
+          switch (keys[1]) {
+            case 'status':
+              return db.Show.findAll({
+                where: {
+                  [Op.and]: [
+                    { network: args.network },
+                    { status: args.status },
+                  ],
+                },
+              })
+            case 'country':
+              return db.Show.findAll({
+                where: {
+                  [Op.and]: [
+                    { network: args.network },
+                    { country: args.country },
+                  ],
+                },
+              })
+          }
+        } else if (keys[0] === 'status') {
+          return db.Show.findAll({
+            where: {
+              [Op.and]: [{ status: args.status }, { country: args.country }],
+            },
+          })
+        }
+      } else {
+        return db.Show.findAll({
+          where: {
+            [Op.and]: [
+              { network: args.network },
+              { status: args.status },
+              { country: args.country },
+            ],
+          },
+        })
+      }
     },
   },
   Mutation: {
